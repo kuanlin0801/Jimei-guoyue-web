244e66bbf4aab88877d81a252c05b30064cce4a3
Kuan Lin <kuan.lin@ecs.com.tw>
2026-06-23 19:18:19 +0800

    K260623K [JimeiGuoyue] Add documents download page.

---
+++ src/data/documents.js
@@ -0,0 +1,5 @@
+export const documents = [
+  { title: '（範例）社團報名表', url: 'https://drive.google.com/REPLACE', type: 'PDF', note: '新團員填寫' },
+  { title: '（範例）樂器調借申請單', url: 'https://drive.google.com/REPLACE', type: 'PDF', note: '' },
+  { title: '（範例）成果發表會通知單', url: 'https://drive.google.com/REPLACE', type: 'PDF', note: '' },
+];
