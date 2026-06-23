5939b2215a41492c97b3f7f7d01d9bacecaf65cf
Kuan Lin <kuan.lin@ecs.com.tw>
2026-06-23 19:18:18 +0800

    K260623J [JimeiGuoyue] Add about page with club intro, teachers, officers.

---
+++ src/data/officers.js
@@ -0,0 +1,6 @@
+export const officers = [
+  { role: '會長', name: '（姓名）', note: '' },
+  { role: '副會長', name: '（姓名）', note: '' },
+  { role: '總務', name: '（姓名）', note: '' },
+  { role: '文書', name: '（姓名）', note: '' },
+];
---
+++ src/data/teachers.js
@@ -0,0 +1,4 @@
+export const teachers = [
+  { name: '（指導老師姓名）', subject: '指揮 / 二胡', bio: '（簡短介紹：經歷、專長）', photo: '' },
+  { name: '（指導老師姓名）', subject: '彈撥', bio: '（簡短介紹）', photo: '' },
+];
